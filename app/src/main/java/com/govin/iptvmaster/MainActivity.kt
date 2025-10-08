cb60d5b3aace68ee9320c8e6dcde29d01c001991
641831416 <168541988+641831416@users.noreply.github.com>
2025-10-08 13:20:03 +0800

    更新

---
+++ app/src/main/java/com/govin/iptvmaster/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.govin.iptvmaster
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
